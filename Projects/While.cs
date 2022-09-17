645d0dc22b95f19a4f39a12e1bebd20fab4ac1c8
Mustafa ali kağan küçük <65673292+malikagan48@users.noreply.github.com>
2022-09-18 00:47:24 +0300

    up

---
+++ Projects/While.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects
+{
+    internal class While
+    {
+        int sayac = 1;
+        while (sayac <= 5)
+        {
+        Console.WriteLine(sayac);
+        sayac++;
+        }
+         int[] sayiDizisi = { 2, 4, 5, 1, 2, 4 };
+         int toplam = 0;
+         foreach(int i in sayiDizisi)
+         {
+          toplam += i ;
+          }
+         }
+          }
