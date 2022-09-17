d6e5d1e92731a44a6a926ba020b743cc8bfd91fb
Mustafa ali kağan küçük <65673292+malikagan48@users.noreply.github.com>
2022-09-18 01:41:53 +0300

    up

---
+++ Projects/Metot.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects
+{
+    internal class Metot
+    {
+        int a = 2;
+        int b = 3;
+        Console.WriteLine(a+b);
+            
+            int sonuc = Topla(a, b);
+        Console.WriteLine(sonuc);
+            
+            metotlar ornek = new metotlar();
+        ornek.EkranaYazdir(sonuc.ToString());
+            
+            
+            
+            
+            int sonuc2 = ornek.ArttırVeTopla(ref a, ref b);//Fonksiyonun referans almak o değişkenin var olduğu adresin yerini belirtiyo olmaktır.Değişken main fonsiyonu tarafından çağırıladığında direk adresten değer dönecektir.
+        ornek.EkranaYazdir(sonuc2.ToString());
+            ornek.EkranaYazdir((a+b).ToString());
+        }
+
+    static int Topla(int deger1, int deger2)//Başlangıç//
+    {
+        return (deger1 + deger2);
+    }
+
+
+
+
+}
+class metotlar
+{
+    public void EkranaYazdir(string veri)//Başlangıçta bu veri prive olarak gelir.
+    {
+        Console.WriteLine(veri);
+    }
+
+    public int ArttırVeTopla(ref int deger1, ref int deger2)
+    {
+        deger1 += 1;
+        deger2 += 1;
+        return deger1 + deger2;
+    }
+}
+}
